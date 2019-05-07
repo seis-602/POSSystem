901dcec1b8ae7199c77efd99562f9bb5d7682355
Surekshya Sharma <shar3415@stthomas.edu>
2019-05-06 19:26:01 -0500

    Added item and Inventory class

---
+++ src/Inventory.java
@@ -0,0 +1,37 @@
+public class Inventory extends Item {
+	private double quantity;
+	private double reorderThreshold;
+	
+	public Inventory(int id, String Iname, String Isupplier, double Iprice, double qty, double rth)
+	{
+		super(id, Iname, Isupplier, Iprice); 
+		this.quantity = qty;
+		this.reorderThreshold = rth;	
+	}
+	
+	public void addInventory(double value) 
+	{
+		this.quantity += value;
+	}
+	
+	public void removeInventory(double value) 
+	{
+		if (value > this.quantity)
+		{
+			//throw error 'Not enough in stock'
+			System.out.println("Not enough in stock");
+		}
+		else
+		{
+			this.quantity -= value;
+		}
+	}
+	
+	public void checkReorder(int value) {
+		if(this.quantity < this.reorderThreshold)
+		{
+			this.quantity += value;
+		}
+	}
+		
+}
